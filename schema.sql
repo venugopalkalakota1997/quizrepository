3aea4cff09a1613fb915af07e9cbb055d8f8ebf0
venugopalreddykalakota1997 <venugopalreddykalakota1997@gmail.com>
2020-06-24 15:04:17 +0530

    ok

--- schema.sql
+++ schema.sql
@@ -1,6 +1,82 @@
+-- MySQL Workbench Forward Engineering
+
 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
+-- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
+-- -----------------------------------------------------
+-- Schema quiz
+-- -----------------------------------------------------
+
+-- -----------------------------------------------------
+-- Schema quiz
+-- -----------------------------------------------------
 CREATE SCHEMA IF NOT EXISTS `quiz` DEFAULT CHARACTER SET utf8 ;
 USE `quiz` ;
+
+-- -----------------------------------------------------
+-- Table `quiz`.`questions`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `quiz`.`questions` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `answer` VARCHAR(255) NULL DEFAULT NULL,
+  `a` VARCHAR(255) NULL DEFAULT NULL,
+  `b` VARCHAR(255) NULL DEFAULT NULL,
+  `c` VARCHAR(255) NULL DEFAULT NULL,
+  `d` VARCHAR(255) NULL DEFAULT NULL,
+  `question` VARCHAR(255) NULL DEFAULT NULL,
+  `fk_quiz_id` INT NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `FK3irea6vu45w04d3jd93duwo72` (`fk_quiz_id` ASC) VISIBLE)
+ENGINE = MyISAM
+AUTO_INCREMENT = 18
+DEFAULT CHARACTER SET = utf8;
+
+
+-- -----------------------------------------------------
+-- Table `quiz`.`quiz`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `quiz`.`quiz` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `quiz_name` VARCHAR(255) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = MyISAM
+AUTO_INCREMENT = 15
+DEFAULT CHARACTER SET = utf8;
+
+
+-- -----------------------------------------------------
+-- Table `quiz`.`school`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `quiz`.`school` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `password` VARCHAR(255) NULL DEFAULT NULL,
+  `school_name` VARCHAR(255) NULL DEFAULT NULL,
+  `username` VARCHAR(255) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = MyISAM
+AUTO_INCREMENT = 3
+DEFAULT CHARACTER SET = utf8;
+
+
+-- -----------------------------------------------------
+-- Table `quiz`.`student`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `quiz`.`student` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `first_name` VARCHAR(255) NULL DEFAULT NULL,
+  `last_name` VARCHAR(255) NULL DEFAULT NULL,
+  `password` VARCHAR(255) NULL DEFAULT NULL,
+  `username` VARCHAR(255) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = MyISAM
+AUTO_INCREMENT = 3
+DEFAULT CHARACTER SET = utf8;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
